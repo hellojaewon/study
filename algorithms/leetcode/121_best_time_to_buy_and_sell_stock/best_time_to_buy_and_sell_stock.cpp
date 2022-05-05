5403a301932bce7cd361322f2545da05d0b782af
hellojaewon <jwchoi.do@gmail.com>
2022-05-05 12:01:25 +0900

    Re-solve best time to buy and sell stock /w linear algorithm

--- algorithms/leetcode/121_best_time_to_buy_and_sell_stock/best_time_to_buy_and_sell_stock.cpp
+++ algorithms/leetcode/121_best_time_to_buy_and_sell_stock/best_time_to_buy_and_sell_stock.cpp
@@ -18,11 +18,28 @@ int dp (vector<int> & prices, int day, bool bought, vector<vector<int>> & memo) 
     return memo[1][day];
 }
 
-int maxProfit(vector<int>& prices) {
+int maxProfitWithDp(vector<int>& prices) {
     vector<vector<int>> memo;
     memo.push_back(vector<int>(prices.size(), -1));
     memo.push_back(vector<int>(prices.size(), -1));
     return dp(prices, 0, false, memo);
+}
+
+int maxProfit(vector<int>& prices) {
+    if (prices.size() < 2) return 0;
+
+    vector<int> buy(prices.size(), -1);
+    vector<int> sell(prices.size(), -1);
+    buy[0] = prices[0];
+    for (int i = 1; i < prices.size(); i++)
+        buy[i] = min(buy[i-1], prices[i]);
+    sell[prices.size() - 1] = prices[prices.size() - 1];
+    for (int i = prices.size() - 2; i >= 0; i--)
+        sell[i] = max(sell[i+1], prices[i]);
+    int maxProfit = 0;
+    for (int i = 0; i < prices.size() - 1; i++)
+        maxProfit = max(maxProfit, sell[i+1]-buy[i]);
+    return maxProfit;
 }
 
 int main () {
